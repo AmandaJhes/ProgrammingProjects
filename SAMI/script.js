e238731aa7172202ed831dea075ed700c32cf608
Tomé Luan <tomeluan05@gmail.com>
2022-04-23 22:50:36 -0300

    acrescentando formulário de dados para ser enviado para o whatsapp

--- SAMI/script.js
+++ SAMI/script.js
@@ -15,3 +15,16 @@ function scroll(){
 function backToTop(){
     document.documentElement.scrollTop = 0;
 }
+
+document.querySelector('#submit').addEventListener('click',function(){
+    let cliente = document.querySelector('#nome').value;
+    let sobrenome = document.querySelector('#sobrenome').value;
+    let email = document.querySelector('#email').value;
+    let telefone = document.querySelector('#telefone').value;
+    
+   let url = ("https://api.whatsapp.com/send?phone=5585986816280&text= Bem vindo a SAMI -->> %0A Qual é o seu nome? %0A " + cliente + " %0A Qual o seu sobrenome ?%0A" + 
+   sobrenome + "%0A Qual o seu e-mail %0A "+ email +" %0A Qual é o seu telefone ? %0A "+ telefone);
+   
+   window.open(url);
+   
+   });
